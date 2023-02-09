ce65885f82564c826d0e7b8c5ceaaa5ea7aff6d8
Esteban <e05072003@gmail.com>
2023-02-09 06:26:33 -0500

    en proceso de implementacion de js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -9,12 +9,12 @@ var aplication = new function () {
 
     var datos = "";
 
-    fetch('http://localhost/Crud_php_and_js/backend/php/empleado')
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.error(error));
+    // fetch(url)
+    //   .then(response => response.json())
+    //   .then(data => console.log(data))
+    //   .catch(error => console.error(error));
 
-    // datos = "<td>Esteban</td><td>e05072003@gmail.com</td><td>masculino</td><td>ventas</td><td>si</td><td></td><td></td>";
+    datos = "<td>Esteban</td><td>e05072003@gmail.com</td><td>masculino</td><td>ventas</td><td>si</td><td></td><td></td>";
 
     return this.tableEmpleados.innerHTML = datos;
   }
